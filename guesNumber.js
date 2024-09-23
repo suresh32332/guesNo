d91acb927e31d1ef31f18b8c95a568748b32e34b
suresh32332 <sureshsuresh32332@gamil.com>
2024-09-23 14:00:47 +0530

    display

---
+++ guesNumber.js
@@ -0,0 +1,23 @@
+var Result = document.getElementById("Result")
+var inputbox = document.getElementById("inputbox")
+function submitButton(){
+    var random = Math.floor(Math.random() * 10 ) + 1
+    console.log(random)
+    let input = parseInt(inputbox.value);
+     Result.style.display= "block"
+
+    if( input <= 9){  
+         if(random==input){
+            Result.innerHTML= "✅Correct..."
+        }
+        else{ 
+          Result.textContent="❗️Incorrect..."
+        }  
+    }
+    else {
+        alert("❌Incorrect. Please enter a number between 1 and 10.")
+        
+    }
+    
+   
+}
